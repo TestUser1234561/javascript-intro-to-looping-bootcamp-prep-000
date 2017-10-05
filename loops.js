8c913f18c4b9460a88ba519176bcbee71db62868
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 03:01:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
   for(i = 0; i < 25; i++) {
     if(i == 0) {
-      array.push(`I am 1 strange loop.`);
+      array.push(`I am 1 strange loop.`)
     } else {
-      array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`)
     }
   }
-  return array;
+  return array
 }
 
 function whileLoop(n) {
@@ -14,16 +14,16 @@ function whileLoop(n) {
     console.log(n)
     n--;
   }
-  return(`done`);
+  return(`done`)
 }
 
 function doWhileLoop(array) {
-  i = array.length;
+  i = array.length
   do {
     delete array[i]
     i--
   } while(array.length > 0 && maybeTrue())
-  return array;
+  return array
 }
 
 function maybeTrue() {
